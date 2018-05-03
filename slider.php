7353079003e134652ab544a221cc9767b125ff74
MJC598 <matthew.j.carroll@outlook.com>
2018-05-03 18:29:26 -0500

    image slider working on auto-rotation

--- slider.php
+++ slider.php
@@ -2,8 +2,6 @@
 
 require_once('template.php');
 
-$images = array('pictures/doggo.jpg', 'pictures/Jesse.jpg', 'pictures/tickets.jpg');
 $tmpl = new Template();
-$tmpl->images = $images;
 $sliderHTML = $tmpl->build('slider.tmpl');
 ?>
