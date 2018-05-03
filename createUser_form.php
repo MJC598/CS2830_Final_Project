debda8d33ee0a77f7aea6e29a1695cd38f5be8fb
MJC598 <matthew.j.carroll@outlook.com>
2018-05-03 18:53:14 -0500

    fixed all pages so they now have the correct header

--- createUser_form.php
+++ createUser_form.php
@@ -1,17 +1,22 @@
+<?php
+require_once('nav.php');
+// Specify the currently selected tab
+$currentPage = 'Login';
+
+// Create a new MenuComponent object (pass it the menu items and the currently selected one)
+$menu = new MenuComponent($menuItems, $currentPage, 'menu.tmpl');
+// Call MenuComponent's 'generate()' method
+$result = $menu->generate();
+$menuHTML = $result['html'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
         <title>Welcome</title>
-        <script src="jslibs/jquery3-3-1.js"></script>
-        <script src="jslibs/angular1-6-9.js"></script>
-        <script src="website.js"></script>
-        <script src="myApp.js"></script>
-        <script src="myController.js"></script>
-<!--        <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">-->
         <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="website.css" />
         <script>
         $(function(){
             $("input[type=submit]").button();	
@@ -32,65 +37,46 @@
         
         });
     </script>
-    </head>
-    <body class="home">
-        <div class="header wrapper">
-            <div class="header-left">Matthew Carroll</div>
-            <div class="header-right">
-                <nav class="nav-bar">
-<!--                    <div>Matthew Carroll</div>-->
-                    <a id="item1" class="nav-bar" href="index.html">Home</a>
-                    <a id="item2" class="nav-bar" href="aboutMe.html">About Me</a>
-                    <a id="item3" class="nav-bar" href="resume.html">Resume</a>
-                    <a id="item4" class="nav-bar" href="projects.html">Projects</a>
-                    <a id="item5" class="nav-bar" href="login.php">Contact Me</a>
-                </nav>
+    <?php 
+        print $menuHTML;
+    ?>
+    <div class="stuff">
+    <?php
+        if ($error) {
+            print "<div class=\"message\">$error</div>\n";
+        }
+    ?>
+    <form name="mattsForm" action="createUser.php" method="POST" >
+        <input type="hidden" name="action" value="do_create" />
+    
+        <div class="item">
+            <label for="firstName">First Name:</label>
+            <input type="text" id="firstName" name="firstName" required>
             </div>
-            <div class="clear"></div>
+        <div class="item">
+            <label for="lastName">Last Name:</label>
+            <input type="text" id="lastName" name="lastName" required>
         </div>
-            <div class="stuff">
-                <?php
-                    if ($error) {
-                        print "<div class=\"message\">$error</div>\n";
-                    }
-                ?>
-                <form name="mattsForm" action="createUser.php" method="POST" >
-                    <input type="hidden" name="action" value="do_create" />
-                    
-                    <div class="item">
-                        <label for="firstName">First Name:</label>
-                        <input type="text" id="firstName" name="firstName" required>
-                    </div>
-                    <div class="item">
-                        <label for="lastName">Last Name:</label>
-                        <input type="text" id="lastName" name="lastName" required>
-                    </div>
-                    <div class="item">
-                        <label for="username">Username:</label>
-                        <input type="text" id="username" name="username" required>
-                    </div>
-                    <div class="item">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" required>
-                    </div>
-                    <div class="item">
-                        <label for="confirmPass">Confirm Password:</label>
-                        <input type="password" id="confirmPass" name="confirmPass" required>
-                    </div>
-                    <div class="item">
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" ng-model="text" required>
-                    </div>
-                    <div class="item">
-                        <input type="submit" value="Submit">
-                    </div>
-                </form>
-                
-            </div>
-<!--
-    <footer class="footer">
-        <p>Copyright (c) 2018</p>
-    </footer>
--->
+        <div class="item">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div class="item">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div class="item">
+            <label for="confirmPass">Confirm Password:</label>
+            <input type="password" id="confirmPass" name="confirmPass" required>
+        </div>
+        <div class="item">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" ng-model="text" required>
+        </div>
+        <div class="item">
+            <input type="submit" value="Submit">
+        </div>
+    </form>
+    </div>
     </body>
 </html>
--- login_form.php
+++ login_form.php
@@ -1,70 +1,53 @@
+<?php
+require_once('nav.php');
+// Specify the currently selected tab
+$currentPage = 'Login';
+
+// Create a new MenuComponent object (pass it the menu items and the currently selected one)
+$menu = new MenuComponent($menuItems, $currentPage, 'menu.tmpl');
+// Call MenuComponent's 'generate()' method
+$result = $menu->generate();
+$menuHTML = $result['html'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
         <title>Login</title>
-        <script src="jslibs/jquery3-3-1.js"></script>
-        <script src="jslibs/angular1-6-9.js"></script>
-        <script src="website.js"></script>
-        <script src="myApp.js"></script>
-        <script src="myController.js"></script>
-        <script src="jslibs/jquery-ui.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
         <script>
             $(function(){
                 $("input[type=submit]").button();
             });
         </script>
-        <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="website.css" />
-        <link rel="stylesheet" type="text/css" href="jslibs/jquery-ui.min.css" />
-    </head>
-    <body class="home">
-        <div class="header wrapper">
-            <div class="header-left">Matthew Carroll</div>
-            <div class="header-right">
-                <nav class="nav-bar">
-<!--                    <div>Matthew Carroll</div>-->
-                    <a id="item1" class="nav-bar" href="index.html">Home</a>
-                    <a id="item2" class="nav-bar" href="aboutMe.html">About Me</a>
-                    <a id="item3" class="nav-bar" href="resume.html">Resume</a>
-                    <a id="item4" class="nav-bar" href="projects.html">Projects</a>
-                    <a id="item5" class="nav-bar" href="login.php">Contact Me</a>
-                </nav>
-            </div>
-            <div class="clear"></div>
+    <?php 
+        print $menuHTML;
+    ?>
+    <div class="content">
+    <?php
+        if ($error) {
+            print "<div class=\"ui-state-error\">$error</div>\n";
+        }
+    ?>
+    <form action="login.php" method="post">
+        <input type="hidden" name="action" value="do_login">
+        <div class="stack">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" class="ui-widget-content ui-corner-all" autofocus value="<?php print $username; ?>">
         </div>
-            <div class="content">
-            <?php
-                if ($error) {
-                    print "<div class=\"ui-state-error\">$error</div>\n";
-                }
-            ?>
-                <form action="login.php" method="post">
-                    <input type="hidden" name="action" value="do_login">
-                    
-                    <div class="stack">
-                        <label for="username">Username:</label>
-                        <input type="text" id="username" name="username" class="ui-widget-content ui-corner-all" autofocus value="<?php print $username; ?>">
-                    </div>
-                    <div class="stack">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" class="ui-widget-content ui-corner-all">
-                    </div>
-            
-                    <div class="stack">
-                        <input type="submit" value="Submit">
-                    </div>
-                    <div class="stack">
-                        <a id="create-user-link" href="createUser_form.php">Not a user?</a>
-                    </div>
-                    
-                </form>
-            </div>
-<!--
-    <footer class="footer">
-        <p>Copyright (c) 2018</p>
-    </footer>
--->
+        <div class="stack">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" class="ui-widget-content ui-corner-all">
+        </div>
+        <div class="stack">
+            <input type="submit" value="Submit">
+        </div>
+        <div class="stack">
+            <a id="create-user-link" href="createUser_form.php">Not a user?</a>
+        </div>
+    </form>
+    </div>
     </body>
 </html>
